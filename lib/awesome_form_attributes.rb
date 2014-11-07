f4ccf4a2b318572d5b6c921c85832aff2432af23
sunzhimeng <zhimeng.sun@qunar.com>
2014-11-07 09:54:49 +0800

    avoid nil error, reset short_used_attrs default values

--- lib/awesome_form_attributes.rb
+++ lib/awesome_form_attributes.rb
@@ -1,4 +1,6 @@
 require "rails"
+require "awesome_form_attributes/version"
+require "awesome_form_attributes/setting"
 require "awesome_form_attributes/helpers"
 require "awesome_form_attributes/used_attr_columns"
 
--- lib/awesome_form_attributes/used_attr_columns.rb
+++ lib/awesome_form_attributes/used_attr_columns.rb
@@ -6,10 +6,10 @@ module UsedAttrColumns
   end
   
   def used_attrs
-    I18n.t("activerecord.attributes.#{self.class.table_name.singularize}").keys
+    I18n.t("activerecord.attributes.#{self.class.table_name.singularize}").try(:keys) || []
   end
 
   def short_used_attrs
-    []
+    used_attrs[0, 5]
   end
 end
